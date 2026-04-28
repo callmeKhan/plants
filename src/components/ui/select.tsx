4b96f64d6b1bf78c9ab25cf798cfe7940c40bdb5
Khanh Nguyen <khanh.nguyen@cast-er.com>
2026-04-28 20:17:04 +0700

    dynamic pot_size

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,15 +1,12 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
-  id: string;
-}
+export interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> { }
 
 const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
-  ({ className, children, id, ...props }, ref) => {
+  ({ className, children, ...props }, ref) => {
     return (
       <select
-        id={id}
         className={cn(
           "flex h-8 w-full rounded-xl border border-gray-200 bg-white px-3 py-0 text-sm transition-colors focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-transparent disabled:cursor-not-allowed disabled:opacity-50 appearance-none cursor-pointer",
           className
